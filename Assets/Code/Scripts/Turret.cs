262fd755ea577549b770f8aacbf74bf3871d1be7
3xA3xA <drainedsimp@gmail.com>
2024-11-30 02:02:34 +0300

    Init

---
+++ Assets/Code/Scripts/Turret.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 3f;
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float bps = 1f; // Bullets Per Second
+
+    private Transform target;
+    private float timeUntilFire;
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else
+        {
+            timeUntilFire += Time.deltaTime;
+
+            if (timeUntilFire >= 1f / bps)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        if (bulletPrefab == null)
+        {
+            Debug.LogError("bulletPrefab is not assigned in the Inspector.");
+            return;
+        }
+
+        if (firingPoint == null)
+        {
+            Debug.LogError("firingPoint is not assigned in the Inspector.");
+            return;
+        }
+
+        Vector3 firingPosition = firingPoint.position;
+        firingPosition.z = 0f; //ќп€ть мен€ю коорд
+
+        GameObject bulletObj = Instantiate(bulletPrefab, firingPosition, Quaternion.identity);
+        Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+
+        if (bulletScript != null)
+        {
+            bulletScript.SetTarget(target);
+        }
+        else
+        {
+            Debug.LogError("Bullet script not found on the instantiated bullet object.");
+        }
+    }
+
+
+    private void RotateTowardsTarget()
+    {
+        float angel = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angel));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)
+        transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+}
